b2fbc31f338f43ee512350bd79a65eaae1c800c9
3idey <elaidya225@gmail.com>
2025-08-23 23:52:06 +0300

    Add : controllers ,Models , Factories , migrations, also 'ide_helper packge' for the the errors that doesnt exist

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Tag;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    protected $model = Tag::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->randomElement([
+                'php',
+                'laravel',
+                'mysql',
+                'javascript',
+                'vue',
+                'react',
+                'api',
+                'backend',
+                'frontend',
+                'database',
+                'html',
+                'css',
+                'tailwind',
+                'bootstrap',
+                'materialize',
+                'design',
+            ]), // WE make it an array its better
+        ];
+    }
+}
